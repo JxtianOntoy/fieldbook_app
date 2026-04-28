c705952b93e49a15de285bec0b9cde2627acb2af
John Ontoy <jxtian1022@gmail.com>
2026-04-28 19:52:46 -0400

    fix pwa manifest paths

--- vite.config.js
+++ vite.config.js
@@ -17,15 +17,16 @@ export default defineConfig({
         background_color: "#f8fafc",
         display: "standalone",
         orientation: "portrait",
-        start_url: "/",
+        start_url: "/fieldbook_app/",
+        scope: "/fieldbook_app/",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/fieldbook_app/icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/icon-512.png",
+            src: "/fieldbook_app/icon-512.png",
             sizes: "512x512",
             type: "image/png"
           }
